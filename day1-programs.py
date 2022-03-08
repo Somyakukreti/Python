439a3bd58471afb787d3f3ce80e22517f29e0684
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-08 18:30:15 +0530

    Add files via upload

---
+++ day1-programs.py
@@ -0,0 +1,47 @@
+# even odd program.
+# n = int(input('Enter a number: '))
+# if n%2==0:
+#     print(f"{n} is an even number")
+# else:
+#     print(f'{n} is an odd number')
+
+
+# Fibonnaci series.
+# n = int(input("Enter the number till which you want the series: "))
+# a = 0
+# b = 1
+# temp=0
+# if n==0:
+#     print(a)
+# elif n==1:
+#     print(b)
+# else:
+#     print(a,end='')
+#     print(b,end='')
+#     for i in range(n):
+#         temp = a+b
+#         a = b
+#         b = temp 
+#         print(temp,end='')
+
+
+# prime number.
+# num = int(input("Enter the number: "))
+# button = False
+# if num>1:
+#     for i in range(2,num):
+#         if (num%i)==0:
+#             button=True
+#             break
+
+# if button:
+#     print("Not a prime number")
+# else:
+#     print("prime number")
+     
+
+# reverse a string.
+# n = input("Enter the name: ")
+# a = len(n)
+# for i in range(a-1,-1,-1):
+#     print(n[i],end='')
