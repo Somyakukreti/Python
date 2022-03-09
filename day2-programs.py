f661e5b5dd1d89733cdb38d35935cac896897fb8
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-09 21:01:55 +0530

    Add files via upload

---
+++ day2-programs.py
@@ -0,0 +1,32 @@
+# even odd lists.
+# checking_list = [1,2,3,4,45,5,42,88,7]
+# even_list = []
+# odd_list = []
+# for i in checking_list:
+#     if i%2==0:
+#         even_list.append(i)
+#     else:
+#         odd_list.append(i)
+
+# print(even_list,odd_list)
+
+
+
+# Normal pattern.
+# lines = int(input("enter the lines: "))
+# for i in range(lines+1):
+#     for j in range(i):
+#         print("*",end='')
+#     print()
+
+
+
+# inverted pattern.
+# lines = int(input("enter the lines: "))
+# n = lines+1
+# for i in range(1,n):
+#     for j in range(n-i):
+#         print("*",end='')
+#     print()
+
+
