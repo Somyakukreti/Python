6198605815c00649f5a2a2fe7c951f633217c4c0
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-10 18:35:07 +0530

    Add files via upload

---
+++ day3-programs.py
@@ -0,0 +1,27 @@
+# greatest of 3 numbers.
+# a = int(input("Enter the number: "))
+# b = int(input("Enter the number: "))
+# c = int(input("Enter the number: "))
+# if a>b and a>c:
+#     print(f"{a} is the greatest")    
+# elif b>a and b>c:
+#     print(f"{b} is the greatest")
+# else:
+#     print(f"{c} is the greatest")    
+
+
+# find simple interest.
+# p = int(input("Enter the principal: "))
+# r = int(input("Enter the rate: "))
+# t = int(input("Enter the time: "))
+# print(f"The Simple interest is {(p*r*t)/100}")
+
+
+# Pattern program.
+# n = int(input("Enter the number: "))
+# for i in range(n+1):
+#     for j in range(n-1):
+#         print(end='')
+#     for j in range(i):
+#         print("*",end='')
+#     print()
