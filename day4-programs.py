d0eefaf1195c0aadb09d88f5b6c8276ebfeb8b5d
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-13 13:54:41 +0530

    Add files via upload

---
+++ day4-programs.py
@@ -0,0 +1,50 @@
+# Number patterns.
+# n = int(input("Enter the number of lines you want to print: "))
+# for i in range(n+1):
+#     for j in range(i):
+#         print(i,end='')
+#     print()    
+
+# dictionary
+# dictionary = {
+#     'Name':'Somya',
+#     'Fav. sport': 'Football',
+#     'Anime':'Naruto'
+# }
+
+# for i in dictionary:
+#     print(i)
+#     print(dictionary[i])
+
+
+
+
+# word = input("Enter the word: ")
+# counting = 0
+# for i in word:
+#     if(i == i.upper()):
+#         counting=counting+1
+#     else:
+#         pass 
+
+# print(f'The upper case letters in the word are {counting}')   
+
+
+# Type casting
+# n = '4'
+# n = int(n)
+# print(type(n))
+
+# dd = '4'
+# hh = 9
+# hh = float(hh)
+# dd = float(dd)
+# print(type(dd),dd)
+# print(type(hh),hh)
+
+# yoyo = True
+# print(yoyo)
+
+# yoyo = int(yoyo)
+# yoyo = False
+# print(type(yoyo),yoyo)
