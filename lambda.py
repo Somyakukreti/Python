1fba7299b623c2662264e952be0099c2ecdbb35b
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-14 18:43:16 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,22 @@
+# def add(a,b):
+#     return a+b
+
+# add2 = lambda a,b : a+b
+# print(add2(2,3))
+
+# multiply = lambda x,y : x*y
+# print(multiply(3,2))
+
+# practice
+# iseven = lambda a:a%2==0
+# print(iseven(7))  
+
+# last_char = lambda a:a[-1]
+# print(last_char('somya'))
+
+
+# if else with lambda.
+# func = lambda s : True if len(s) > 5 else False
+# print(func('somyaa'))
+# func2=lambda s:len(s)>5
+# print(func2('somya'))
