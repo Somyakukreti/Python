33b87ed0878b88838e3b0175b926a9799b93e806
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-15 18:35:04 +0530

    Add files via upload

---
+++ day5-programs.py
@@ -0,0 +1,24 @@
+# Palindrome.
+# number = int(input("Enter the number: "))
+# temp = number
+# reverse = 0
+
+# while number>0:
+#     dig = number%10
+#     reverse=reverse*10+dig
+#     number = number//10
+
+# if temp==reverse:
+#     print("The number is a palindrome")
+# else:
+#     print("The number is not a palindrome")   
+
+
+# Reversing a list.
+# lists = [1,2,3,4,66,22]
+# reversed_list=[]
+# n = len(lists)-1
+# for i in range(n,-1,-1):
+#     reversed_list.append(lists[i])
+
+# print(reversed_list)    
