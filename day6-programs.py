160a21a82af647aa6391429cb7e970c042247ef9
Somyakukreti <81754493+Somyakukreti@users.noreply.github.com>
2022-03-22 18:44:54 +0530

    Add files via upload

---
+++ day6-programs.py
@@ -0,0 +1,33 @@
+# Sorting a list.
+# mylist = [2,1,4,5,3,6,7]
+# for i in range(len(mylist)-1):
+#     for j in range(i):      
+#         if mylist[j] > mylist[j+1]:
+#             temp = mylist[j]
+#             mylist[j] = mylist[j+1]
+#             mylist[j+1] = temp
+# print(mylist)
+
+
+# counting the characters in a string.
+# n = 'shaarkk'
+# a = input("Enter the character the occurance of which you want to know: ")
+# counts=0
+# for i in n:
+#     if i==a:
+#         counts += 1 
+#     else:
+#         pass 
+# print(f"{a} comes {counts} times in the string")   
+
+
+# Counting number of vowels in a sentence.
+# sentence = input("Enter the sentence: ")
+# string = sentence.lower()
+# counts = 0
+# list1=['a','e','i','o','u']
+# for i in string:
+#     if i in list1:
+#         counts = counts+1
+
+# print(f"Number of vowels in the sentence is {counts}")
